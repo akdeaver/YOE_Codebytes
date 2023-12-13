54b59cc5a3f861fe393b1a1f9ebde5a0510961a7
Alex Deaver <alex-de@EMCINS.COM>
2023-12-13 14:15:22 -0600

    fixup for json formatting

--- Wk_12/Dynamo_Query_test.py
+++ Wk_12/Dynamo_Query_test.py
@@ -14,6 +14,7 @@ def use_get_teams_for_list():
     url = f"{base_url}/teams"
     response = requests.get(url)
 
+    #Assert the response status code is 200
     assert response.status_code == 200
 
 def test_get_team_endpoint():
@@ -21,6 +22,7 @@ def test_get_team_endpoint():
     url = f"{base_url}/teams"
     all_teams = requests.get(url).json()
 
+    #Loop over teams and verify each is getting a 200
     for teams in all_teams:
         url = f"{base_url}/team?team_name={teams}"
         response = requests.get(url)
