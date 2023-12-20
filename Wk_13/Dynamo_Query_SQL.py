c6ffef8a92996610cf211aae3e4d80889119d8dd
Alex Deaver <alex-de@EMCINS.COM>
2023-12-20 11:43:27 -0600

    Updating tests and adding methods

--- Wk_13/Dynamo_Query_SQL.py
+++ Wk_13/Dynamo_Query_SQL.py
@@ -12,12 +12,11 @@ You can call /employees /teams /team?team_name={team_name}
 '''
 
 #Grabs ssm parameters needed to run query
-#Thanks for brandon-m for the "['Parameter']['Value']" to shorten the number of lines
 ssm = boto3.client('ssm', aws_region)
 cluster_name = ssm.get_parameter(Name='/training10/techops/dsd/exercise12/cluster', WithDecryption=True)['Parameter']['Value']
 db_name = ssm.get_parameter(Name='/training10/techops/dsd/exercise12/database', WithDecryption=True)['Parameter']['Value']
-db_pw = ssm.get_parameter(Name='/training10/techops/dsd/exercise12/password', WithDecryption=True)['Parameter']['Value']
-db_uname = ssm.get_parameter(Name='/training10/techops/dsd/exercise12/username', WithDecryption=True)['Parameter']['Value']
+db_pw = ssm.get_parameter(Name='/training10/techops/dsd/exercise13/alex', WithDecryption=True)['Parameter']['Value']
+db_uname = "ex13_alex_rw"
 
 db_parameters = {
     'host': cluster_name,
@@ -31,12 +30,11 @@ def get_teams():
     try:
         # Connect to the PostgreSQL server
         conn = psycopg2.connect(**db_parameters)
-
         # Create a cursor object to execute queries
         cur = conn.cursor()
 
         # Execute the query
-        cur.execute("SELECT team_name FROM public.teams;")
+        cur.execute("SELECT team_name FROM public.ex13_alex_teams;")
         all_teams = cur.fetchall()
 
         #close connections
@@ -54,12 +52,11 @@ def get_employees():
     try:
         # Connect to the PostgreSQL server
         conn = psycopg2.connect(**db_parameters)
-
         # Create a cursor object to execute queries
         cur = conn.cursor()
 
         # Execute the query
-        cur.execute("SELECT first_name, last_name, title, team_name FROM public.employees LEFT JOIN public.teams ON public.teams.id = public.employees.team_id;")
+        cur.execute("SELECT first_name, last_name, title, team_name FROM public.ex13_alex_employees LEFT JOIN public.ex13_alex_teams ON public.ex13_alex_teams.id = public.ex13_alex_employees.team_id;")
         result = cur.fetchall()
 
         #Close connections and return result
@@ -69,6 +66,60 @@ def get_employees():
 
     except (Exception, psycopg2.DatabaseError) as error:
         print(f"Error: {error}")
+
+def add_employee(first_name, last_name, title, team_name, date_of_birth, date_of_service):
+    # Connect to the PostgreSQL server
+    conn = psycopg2.connect(**db_parameters)
+    # Create a cursor object to execute queries
+    cur = conn.cursor()
+
+    try:
+        #Saving to improve in the future
+        '''INSERT INTO table2 SELECT col1,col2 FROM table1 t1 WHERENOT EXISTS( SELECT 1 FROM table2 t2 WHERE t1.col1 = t2.col1 AND t1.col2 = t2.col2)'''
+
+        #Query to find team id to match team name input
+        team_id_query = "SELECT id FROM public.ex13_alex_teams WHERE team_name = %s;"
+        cur.execute(team_id_query, (team_name,))
+        team_id = cur.fetchone()
+
+        #Query to insert new user
+        insert_query = """
+            INSERT INTO public.ex13_alex_employees (first_name, last_name, title, team_id, date_of_birth, date_of_service)
+            VALUES (%s, %s, %s, %s, %s, %s);
+        """
+
+        # Execute the query
+        cur.execute(insert_query, (first_name, last_name, title, team_id, date_of_birth, date_of_service))
+        conn.commit()
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(f"Error: {error}")
+
+    finally:
+        #Close connections and return result
+        cur.close()
+        conn.close()
+
+def delete_employee(first_name, last_name):
+    # Connect to the PostgreSQL server
+    conn = psycopg2.connect(**db_parameters)
+    # Create a cursor object to execute queries
+    cur = conn.cursor()
+
+    try:
+        #Query to delete user when matches first name and last name
+        sql = "Delete FROM public.ex13_alex_employees WHERE first_name = %s AND last_name = %s;"
+        # Execute the query
+        cur.execute(sql, (first_name, last_name))
+        conn.commit()
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(f"Error: {error}")
+
+    finally:
+        #Close connections and return result
+        cur.close()
+        conn.close()
 
 def get_teams_employees(team_name):
     try:
@@ -79,7 +130,7 @@ def get_teams_employees(team_name):
         cur = conn.cursor()
 
         # Execute the query
-        cur.execute("SELECT first_name, last_name, title, team_name FROM public.employees LEFT JOIN public.teams ON public.teams.id = public.employees.team_id WHERE team_name = %s", (team_name,))
+        cur.execute("SELECT first_name, last_name, title, team_name FROM public.ex13_alex_employees LEFT JOIN public.ex13_alex_teams ON public.ex13_alex_teams.id = public.ex13_alex_employees.team_id WHERE team_name = %s", (team_name,))
         result = cur.fetchall()
 
         #Close connections and return result
@@ -90,18 +141,110 @@ def get_teams_employees(team_name):
     except (Exception, psycopg2.DatabaseError) as error:
         print(f"Error: {error}")
 
+def remove_team(team_name):
+    # Connect to the PostgreSQL server
+    conn = psycopg2.connect(**db_parameters)
+    # Create a cursor object to execute queries
+    cur = conn.cursor()
+
+    try:
+        # Execute the query
+        cur.execute("Delete FROM public.ex13_alex_teams WHERE team_name = %s", (team_name,))
+        conn.commit()      
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(f"Error: {error}")
+
+    finally:
+        #Close connections and return result
+        cur.close()
+        conn.close()
+
+def add_team(team_name):
+    # Connect to the PostgreSQL server
+    conn = psycopg2.connect(**db_parameters)
+    # Create a cursor object to execute queries
+    cur = conn.cursor()
+
+    try:
+        #Query to add team to values
+        #Should check for existing first
+        SQL = "INSERT INTO public.ex13_alex_teams (team_name) VALUES (%s);"
+        data = (team_name,)
+        # Execute the query
+        cur.execute(SQL, data)
+        conn.commit()
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(f"Error: {error}")
+
+    finally:
+        #Close connections and return result
+        cur.close()
+        conn.close()
+
+def update_team(old_team, team_name):
+    # Connect to the PostgreSQL server
+    conn = psycopg2.connect(**db_parameters)
+    # Create a cursor object to execute queries
+    cur = conn.cursor()
+
+    try:
+        #Query to update the team name to new name
+        SQL = "UPDATE public.ex13_alex_teams SET team_name = %s WHERE team_name = %s;"
+        data = (team_name, old_team)
+        # Execute the query
+        cur.execute(SQL, data)
+        conn.commit()
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(f"Error: {error}")
+
+    finally:
+        #Close connections and return result
+        cur.close()
+        conn.close()
+
+
 #Flask App
 app = Flask(__name__)
 
 #App Route for /employees
-@app.route('/employees', methods=['GET'])
-def get_employees_api():
-    #Run get get_employees definition to list all employees
-    result = get_employees()
+@app.route('/employees', methods=['GET', 'POST', 'DELETE'])
+def employees_api():
+    if request.method == 'GET':
+        #Run get get_employees definition to list all employees
+        result = get_employees()
 
-    #Form data from list of lists to json output
-    json_result = [{"first_name": item[0], "last_name": item[1], "title": item[2], "team": item[3]} for item in result]
-    return jsonify({"employees": json_result}), 200
+        #Form data from list of lists to json output
+        json_result = [{"first_name": item[0], "last_name": item[1], "title": item[2], "team_name": item[3]} for item in result]
+        return jsonify({"employees": json_result}), 200
+    
+    if request.method == 'POST':
+        first_name = request.args.get('first_name')
+        last_name = request.args.get('last_name')
+        title = request.args.get('title')
+        team_name = request.args.get('team_name')
+        date_of_birth = request.args.get('date_of_birth')
+        date_of_service = request.args.get('date_of_service')
+
+        #Invoke add employee
+        result = add_employee(first_name, last_name, title, team_name, date_of_birth, date_of_service)
+
+        #Form data from list of lists to json output
+        json_result = {"added": {"first_name": first_name, "last_name": last_name, "team_name": team_name }}
+        return jsonify({"employee": json_result}), 200
+
+    if request.method == 'DELETE':
+        first_name = request.args.get('first_name')
+        last_name = request.args.get('last_name')
+        
+        #invoke delete employee
+        result = delete_employee(first_name, last_name)
+
+        #Form data from list of lists to json output
+        json_result = {"deleted": {"first_name": first_name, "last_name": last_name }}
+        return jsonify({"employee": json_result}), 200
 
 #App Route for /teams
 @app.route('/teams', methods=['GET'])
@@ -111,17 +254,44 @@ def get_teams_api():
     return result, 200
 
 #App Route for /team
-@app.route('/team', methods=['GET'])
-def get_team_api():
-    #Get team name input and run get_teams_employees definition
-    team_name = request.args.get('team_name')
-    result = get_teams_employees(team_name)
+@app.route('/team', methods=['GET', 'DELETE', 'POST', 'PUT'])
+def team_api():
+    if request.method == 'GET':
+        #Get team name input and run get_teams_employees definition
+        team_name = request.args.get('team_name')
+        result = get_teams_employees(team_name)
 
-    #Form data from list of lists to json output
-    json_result = [{"first_name": item[0], "last_name": item[1], "title": item[2]} for item in result]
-    return jsonify({team_name : json_result}), 200
+        #Form data from list of lists to json output
+        json_result = [{"first_name": item[0], "last_name": item[1], "title": item[2]} for item in result]
+        return jsonify({team_name : json_result}), 200
+    
+    if request.method == 'DELETE':
+        #Take team name and remove from db
+        team_name = request.args.get('team_name')
+        result = remove_team(team_name)
 
-    #return result, 200
+        #Form data from list of lists to json output
+        json_result = {"deleted": team_name }
+        return jsonify({"team": json_result}), 200
+    
+    if request.method == 'POST':
+        #take new team and add to db
+        team_name = request.args.get('team_name')
+        result = add_team(team_name)
+
+        #Form data from list of lists to json output
+        json_result = {"added": team_name }
+        return jsonify({"team": json_result}), 200
+    
+    if request.method == 'PUT':
+        #take new team and add to db
+        team_name = request.args.get('team_name')
+        old_team = request.args.get('old_team')
+        result = update_team(old_team, team_name)
+
+        #Form data from list of lists to json output
+        json_result = {"updated": team_name }
+        return jsonify({"team": json_result}), 200
 
 if __name__ == '__main__':
     app.run(port=8000)
--- Wk_13/Dynamo_Query_test.py
+++ Wk_13/Dynamo_Query_test.py
@@ -2,6 +2,45 @@ import pytest
 import requests
 
 base_url = "http://127.0.0.1:8000"
+all_teams = [
+    "Cloud Engineering",
+    "Communication Engineering",
+    "DevOps",
+    "Mainframe",
+    "Networking",
+    "Site Reliability Engineering",
+    "System Support Engineering"
+]
+all_employees =  [
+    {"last_name": "ehlis", "first_name": "adam", "dob": "01/01/1975", "title": "Platform Engineering Manager", "team_name": "Cloud Engineering", "dos": "06/13/2020"},
+    {"last_name": "botts", "first_name": "john", "dob": "12/01/1980", "title": "Platform Engineer III", "team_name": "Site Reliability Engineering", "dos": "06/13/2001"},
+    {"last_name": "deaver", "first_name": "alex", "dob": "12/01/2000", "title": "Platform Engineer III", "team_name": "Communication Engineering", "dos": "08/01/2021"},
+    {"last_name": "graber", "first_name": "mark", "dob": "09/01/1978", "title": "Senior Platform Engineer", "team_name": "Site Reliability Engineering", "dos": "03/01/2000"},
+    {"last_name": "green", "first_name": "damien", "dob": "10/01/1981", "title": "Senior Network Engineer", "team_name": "Cloud Engineering", "dos": "10/01/2017"},
+    {"last_name": "kolbe", "first_name": "scott", "dob": "10/01/1990", "title": "Network Engineer II", "team_name": "Networking", "dos": "06/01/2019"},
+    {"last_name": "marcotte", "first_name": "brandon", "dob": "11/01/1989", "title": "Platform Engineer III", "team_name": "Communication Engineering", "dos": "08/01/2018"},
+    {"last_name": "monroe", "first_name": "tiffany", "dob": "02/01/1998", "title": "System Support Engineer II", "team_name": "System Support Engineering", "dos": "12/01/2020"},
+    {"last_name": "morris", "first_name": "tom", "dob": "02/01/1976", "title": "Platform Engineer III", "team_name": "Site Reliability Engineering", "dos": "02/01/2017"},
+    {"last_name": "trujillo", "first_name": "michael", "dob": "05/01/1989", "title": "System Support Engineer II", "team_name": "System Support Engineering", "dos": "09/01/2018"},
+    {"last_name": "walker", "first_name": "philip", "dob": "10/01/1965", "title": "Network Engineer II", "team_name": "Networking", "dos": "01/01/1995"}
+]
+
+def test_creating_teams_endpoint():
+    for teams in all_teams:
+        url = f"{base_url}/team?team_name={teams}"
+        response = requests.post(url)
+
+        #Assert the response status code is 200
+        assert response.status_code == 200
+
+def test_creating_employees_endpoint():
+    for employee in all_employees:
+        url = f"{base_url}/employees?last_name={employee['last_name']}&first_name={employee['first_name']}&date_of_birth={employee['dob']}&title={employee['title']}&team_name={employee['team_name']}&date_of_service={employee['dos']}"
+        # Make the POST request
+        response = requests.post(url)
+
+        # Assert the response status code is 200
+        assert response.status_code == 200
 
 def test_employee_endpoint():
     url = f"{base_url}/employees"
@@ -10,9 +49,10 @@ def test_employee_endpoint():
     #Assert the response status code is 200
     assert response.status_code == 200
 
-def use_get_teams_for_list():
+def test_use_get_teams_for_list():
     url = f"{base_url}/teams"
     response = requests.get(url)
+    print("testing teams")
 
     #Assert the response status code is 200
     assert response.status_code == 200
@@ -26,6 +66,22 @@ def test_get_team_endpoint():
     for teams in all_teams:
         url = f"{base_url}/team?team_name={teams}"
         response = requests.get(url)
+
+        #Assert the response status code is 200
+        assert response.status_code == 200
+
+#post cleanup when needed
+def test_post_cleanup():
+    for teams in all_teams:
+        url = f"{base_url}/team?team_name={teams}"
+        response = requests.delete(url)
+
+        #Assert the response status code is 200
+        assert response.status_code == 200
+
+    for employee in all_employees:
+        url = f"{base_url}/employees?last_name={employee['last_name']}&first_name={employee['first_name']}"
+        response = requests.delete(url)
 
         #Assert the response status code is 200
         assert response.status_code == 200
